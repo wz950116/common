8b0ad24a6c402f80eb13aabe67c224065a7b9b1a
wz950116 <wz950116@gmail.com>
2023-05-17 19:30:22 +0800

    20230519

--- base-web-cli/src/components/Tinymce/index.vue
+++ base-web-cli/src/components/Tinymce/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tinymce-editor">
-    <Editor :id="editorId" v-model="editorValue" :init="editorInit" :disabled="disabled" @onClick="handleClick" />
+    <Editor :id="editorId" :init="editorInit" :disabled="disabled" @onClick="handleClick" />
   </div>
 </template>
 
@@ -16,6 +16,7 @@ import 'tinymce/themes/silver/theme.min.js'
 import 'tinymce/skins/ui/oxide/skin.min.css'
 // 扩展插件
 import 'tinymce/plugins/image'
+import 'tinymce/plugins/media'
 import 'tinymce/plugins/link'
 import 'tinymce/plugins/code'
 import 'tinymce/plugins/table'
@@ -25,6 +26,10 @@ import 'tinymce/icons/default'
 export default {
   name: 'TinymceEditor',
   components: { Editor },
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
   props: {
     id: {
       type: String,
@@ -44,7 +49,7 @@ export default {
     },
     toolbar: {
       type: [String, Array],
-      default: 'fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | undo redo | link unlink image code | removeformat'
+      default: 'fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | undo redo | link unlink image media code | removeformat'
     }
   },
   data() {
@@ -61,15 +66,48 @@ export default {
         branding: false, // 水印“Powered by TinyMCE”
         images_upload_handler: (blobInfo, success, failure) => {
           this.$emit('handleImgUpload', blobInfo, success, failure)
+        },
+        file_picker_callback: (callback, value, meta) => {
+          if (meta.filetype === 'media' || meta.filetype === 'image') {
+            // 模拟本地上传
+            const input = document.createElement('input')
+            input.setAttribute('type', 'file')
+            if (meta.filetype === 'image') {
+              input.setAttribute('accept', 'image/*')
+            } else {
+              input.setAttribute('accept', 'video/*')
+            }
+            input.onchange = function () {
+              const file = this.files[0]
+              const formData = new FormData()
+              formData.append('file', file)
+              // request({
+              //   method: 'post',
+              //   url: '/ps/common/file/upload',
+              //   data: formData,
+              //   processData: false, // 告诉axios不要去处理发送的数据(重要参数)
+              //   contentType: false // 告诉axios不要去设置Content-Type请求头
+              // }).then((res) => {
+              //   if (res.success) {
+              //     callback(res.data.fileUrl)
+              //   }
+              // })
+            }
+            input.click()
+          }
+        },
+        init_instance_callback: (editor) => {
+          // 确保接口返回数据后再显示编辑器
+          if (this.value) {
+            editor.setContent(this.value)
+          }
+          editor.on('NodeChange Change KeyUp SetContent', () => {
+            this.$emit('input', editor.getContent())
+          })
         }
       },
       editorId: this.id,
       editorValue: this.value
-    }
-  },
-  watch: {
-    editorValue(newValue) {
-      this.$emit('input', newValue)
     }
   },
   mounted() {
@@ -81,6 +119,7 @@ export default {
     },
     clear() {
       this.editorValue = ''
+      this.$emit('input', '')
     }
   }
 }
